92a7044e5146c82ee0ef347a6ceaa5775b4dd33c
Max Bernstein <tekknolagi@gmail.com>
2013-01-16 21:38:32 -0800

    conf

---
+++ lib/dbconfig.rb
@@ -0,0 +1,2 @@
+#DataMapper.setup :default, 'mysql://root:huskiesaresocute@localhost/brightswipedev'
+DataMapper.setup :default, 'postgres://tdgwdmxmgnvrjy:lgCkKM8w2QvE6mxDJ85BroPq8N@ec2-107-22-170-67.compute-1.amazonaws.com/dcr1848vkguan7'
