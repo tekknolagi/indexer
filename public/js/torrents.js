4bd59485751e5514b7bf48e81f234a54e66504e3
Max Bernstein <tekknolagi@gmail.com>
2013-01-21 11:36:45 -0800

    downloads can be singular

--- public/js/torrents.js
+++ public/js/torrents.js
@@ -1,7 +1,7 @@
 $.getJSON("/api/"+$page, function(data) {
     console.log(data);
     $.each(data['contents'], function(key,val) {
-	$(".results ol").append("<li><div class='actions'><a href='/magnet/"+val["id"]+"'><button class='btn-magnet'><img src='/img/magnet-icon.png' />Magnet</button></a></div>"+"<div class='name'>"+val['name']+"</div>"+"<div class='meta'><span class='downloads'>"+val["downloads"]+" downloads</span><span>&nbsp;|&nbsp;</span><span class='date'>Uploaded on "+new Date(Date.parse(val['created_at'])).toLocaleDateString()+"</span></div></li>");
+	$(".results ol").append("<li><div class='actions'><a href='/magnet/"+val["id"]+"'><button class='btn-magnet'><img src='/img/magnet-icon.png' />Magnet</button></a></div>"+"<div class='name'>"+val['name']+"</div>"+"<div class='meta'><span class='downloads'>"+val["downloads"]+" download"+((parseInt(val["downloads"]) > 1)?"s":"")+"</span><span>&nbsp;|&nbsp;</span><span class='date'>Uploaded on "+new Date(Date.parse(val['created_at'])).toLocaleDateString()+"</span></div></li>");
 	console.log("ELEMENT MADE: "+val['name']);
     });
 });
